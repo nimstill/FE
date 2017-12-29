1bbda5ae7a04d7c31c4233954a8cb40bad7189a0
nimstill <dandelionstill@gmail.com>
2017-12-29 22:24:06 +0800

    vue

---
+++ Vue.vue
@@ -0,0 +1,38 @@
+// mixins
+
+var mixin = {
+    created: function () {
+        console.log("mixin");
+    },
+    methods: {
+        foo: function() {
+            console.log('foo');
+        },
+        conflicting: function() {
+            console.log('from mixin');
+        }
+    }
+}
+
+
+Vue.directive('focus', {
+    bind: function() {
+
+    },
+    inserted: function (el) {
+
+    },
+    update: 
+
+    compinentUpdated: 
+
+    unbind: 
+
+
+})
+
+<span>Message: {{ result }}</span>
+
+
+
+
