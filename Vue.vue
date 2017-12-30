f7c02c4c2c6cf08ca51ee4e3b895eb284c546797
nimstill <dandelionstill@gmail.com>
2017-12-30 19:22:03 +0800

    vuex

--- Vue.vue
+++ Vue.vue
@@ -34,5 +34,75 @@ Vue.directive('focus', {
 <span>Message: {{ result }}</span>
 
 
+<form v-on:submit.prevent='onSubmit'></form>
+
+<p v-bind:attrOrProp
+    .prop = ''
+    .camel = ''
+    .sync
+></p>
+
+Vue.component('my-component', Vue.extend({ }))
+
+Vue.component('my-component', { })
+
+var myComponent = Vue.component = Vue.component('my-component')
+
+Vue.component('simple-counter', {
+    template: '<button v-on:click="counter += 1"'>{{ counter }}</button>',
+    date: function () {
+        return {
+            a: '',
+            b: ''
+        }
+    }
+})
+
+
+父子组件之间的通信
+props down , events up 
+
+
+// 函数化组件
+
+Vue.component('my-component',  {
+    functional: true,
+    render: function ( createElement, context) {},
+    props: {}
+})
+
+<script>
+    var vm = new Vue({
+        el: "#example",
+        date: {
+            currentView: "home"
+        },
+        components: {
+            home: {},
+            posts: {},
+            archive: {}
+        }
+    })
+</script>
+
+<component v-bind:is="currentView">
+</component>
+
+
+const store = new Vuex.Store({
+    state: {
+        conut: 0
+    },
+    mutations: {
+        increment (state) {
+            state.count++
+        }
+    }
+})
+
+store.commit("increment")
+
+console.log(store.state.count)
+
 
 
