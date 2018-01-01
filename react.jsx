cbe2c562379189a88ef2d5aca74832bb66929ecf
nimstill <dandelionstill@gmail.com>
2018-01-01 17:04:18 +0800

    react

--- react.jsx
+++ react.jsx
@@ -28,7 +28,7 @@ function impure(firstname, lastname) {
 }
 
 
-### 向类组件添加state
+//### 向类组件添加state
 
 class Clock extends React.Component {
     render() {
@@ -80,6 +80,11 @@ class Toggle extends React.Compinent {
     constructor(props) {
         super(props);
         this.state = {isToggleOn: true};
+        /** 
+         * constructor()构造函数当中，this.handleClick.bind(this)的含义。
+         * 使用bind()是为了将类组件的this作用域绑定至指定函数，从而方便的在该函数内部使用this操作React类组件上的其它方法。
+         * 
+         */
 
         this.handleClick = this.handleClick.bind(this);
     }
@@ -103,3 +108,81 @@ ReactDOM.render(
     <Toggle />,
     document.getElementById('app')
 );
+
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+        <div>
+            <h1>Hello!</h1>
+            {unreadMessages.length > 0 &&
+              <h2>
+                  YOu have {unreadMessages.length} unread Messages.
+              </h2>
+            }
+        </div>
+    );
+}
+
+const messages = ['React', 'Re: react']
+ReactDOM.render(
+    <Mailbox unreadMessages={messages} />,
+    document.getElementById('app')
+);
+
+
+ class Page extends React.Component {
+     constructor(props) {
+         super(props);
+         this.state = {showWarning: true}
+         this.handleToggleClick = this.handleClick.bind(this);
+     }
+
+     handleToggleClick() {
+        this.setState(prevState => ({
+          showWarning: !prevState.showWarning
+        }));
+      }
+      render() {
+        return (
+          <div>
+            <WarningBanner warn={this.state.showWarning} />
+            <button onClick={this.handleToggleClick}>
+              {this.state.showWarning ? '隐藏' : '显示'}
+            </button>
+          </div>
+        );
+    }
+ }
+ ReactDOM.render(
+     <Page />
+     document.getElementById('app')
+ );
+
+const numbers = [1, 1, 1, 1, 1];
+const doubled = numbers.map((number) => number * 2);
+
+const listItems = numbers.map((number) => 
+    <li>{number}</li>
+);
+ReactDOM.render(
+    <ul>{listItems}</ul>,
+    document.getElementById('app')
+);
+
+function NumberList(props) {
+    const numbers = props.numbers;
+    const listItems = numbers.map((number) =>
+      <li>{number}</li>
+    );
+    return {
+        <ul>{listItems}</ul>
+    };
+}
+
+const numbers = [1, 2, 3, 4, 5];
+ReactDOM.render(
+    <NumberList numbers={numbers} />,
+    doucment.getElementById('app')
+);
+
+
