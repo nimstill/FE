65bc5b16a94d918dcf75a7d352549c09496b65be
nimstill <dandelionstill@gmail.com>
2018-01-23 17:03:39 +0800

    react

--- react.jsx
+++ react.jsx
@@ -688,5 +688,78 @@ location / {
 
 
 
-Last-Modified/If-Modified-Since 与 ETag/If-None-Match
+// Last-Modified/If-Modified-Since 与 ETag/If-None-Match
+
+// State and Lifecycle
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>Hello, world</h1>
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
+    </div>
+  );
+}
+
+class Clock extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
+function tick() {
+  ReactDOM.render(
+    <Clock date={new Date()} />,
+    document.getElementById('root')
+  );
+}
+
+setInterval(tick, 1000);
+
+
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  compinentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+
+  compinentWillUnmount() {
+    clearInterval(this.timerID);
+  }
+
+  tick() {
+    this.setState({
+      date: new Date()
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString}</h2>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Clock />,
+  document.getElementById('root')
+);
+
+
 
